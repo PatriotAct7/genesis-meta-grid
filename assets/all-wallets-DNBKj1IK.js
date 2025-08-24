f8f924bfd4109574d6b64b0d91777e143e18babe
Student <you@example.com>
2025-08-24 09:04:55 -0400

    Updates

---
+++ assets/all-wallets-DNBKj1IK.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-D6WmX2oY.js";import"./index-C-tsLbER.js";import"./index.es-DL5cKoHv.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{p as allWalletsSvg};
